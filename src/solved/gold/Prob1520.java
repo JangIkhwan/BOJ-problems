e23174e72b3e16ec3e226630f2dedfa2bea180ea
JangIkhwan <bluensky0213@gmail.com>
2024-12-21 17:35:45 +0900

    24/12/21 문제 풀이

--- src/solved/gold/Prob1520.java
+++ src/solved/gold/Prob1520.java
@@ -9,23 +9,37 @@ import java.util.StringTokenizer;
 public class Prob1520 {
     /*
 
-    24/12/21 15:25 ~
+    24/12/21 15:25 ~ 17:00
 
-    dfs() ?
+    유형 : dfs, DP
+
+    풀이 과정
+
+    1. dfs()로 경로 개수 세기
     모든 경로 찾을때까지 반복
     시간 초과 발생...
 
-    dp를 이용 ?
+    2. dp를 이용하여 최적화
+    이미 목적지로 가는 경로의 수가 계산된 칸이 있다면,
+     그 칸을 방문하지 않아도 저장된 값으로 현재 칸에서 목적지로 가는 경로의 수를 계산할 수 있음
+
     dp[i][j] : (i, j) 칸에서 목적지로 갈 수 있는 경우의 수
 
-    이 칸에 도달했을 때 다음에 이동할 i,j 칸이 계산되어 있다면 answer += d[i][j]
+    현재 칸에 저장할 dp값이 routes라고 하자.
+    다음 칸으로 가는 값이 계산되어 있다면, routes += dp[i][j]
+    그렇지 않다면, routes += dfs(i, j)
 
+    전체 답은 dp[1][1]에 저장된다.
+
+예시 입력
 
 4 5
 50 45 37 32 30
 35 50 40 20 25
 30 29 24 17 28
 27 24 22 15 10
+
+답 : 6
 
      */
     private static int M, N, answer = 0;
@@ -51,12 +65,11 @@ public class Prob1520 {
             }
         }
         dfs(1, 1);
-        System.out.println(answer);
+        System.out.println(dp[1][1]);
     }
 
     public static int dfs(int curX, int curY){
         if(curX == N && curY == M) {
-            answer++;
             return 1;
         }
         int reachableroutes = 0;
@@ -67,7 +80,6 @@ public class Prob1520 {
             if(map[ny][nx] >= map[curY][curX]) continue;
             if(dp[ny][nx] != -1){
                 reachableroutes += dp[ny][nx];
-                answer += dp[ny][nx];
             }
             else{
                 if(!visited[ny][nx]){
@@ -79,22 +91,5 @@ public class Prob1520 {
         }
         dp[curY][curX] = reachableroutes;
         return reachableroutes;
-    }
-
-    public static void dfs0(int curX, int curY){
-        if(curX == N && curY == M) {
-            answer++;
-        }
-        for(int direct = 0; direct < 4; direct++){
-            int nx = curX + dx[direct];
-            int ny = curY + dy[direct];
-            if(nx < 1 || nx > N || ny < 1 || ny > M) continue;
-            if(map[ny][nx] >= map[curY][curX]) continue;
-            if(!visited[ny][nx]){
-                visited[ny][nx] = true;
-                dfs0(nx, ny);
-                visited[ny][nx] = false;
-            }
-        }
     }
 }
