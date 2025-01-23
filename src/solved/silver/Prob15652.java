2111b7c0f8341a5655b42177a451600101c663cb
JangIkhwan <bluensky0213@gmail.com>
2025-01-23 10:18:34 +0900

    24/12/22 15652 문제 해결

---
+++ src/solved/silver/Prob15652.java
@@ -0,0 +1,46 @@
+package solved.silver;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Prob15652 {
+    /*
+    https://www.acmicpc.net/problem/15652
+
+    11:49 ~ 11:53 12:00 ~ 12:10
+    */
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int N, M;
+    private static int[] sequence;
+
+    public static void main(String[] args) throws IOException {
+        String line = br.readLine();
+        StringTokenizer st = new StringTokenizer(line);
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        sequence = new int[M + 1];
+        dfs(1);
+        bw.flush();
+    }
+
+    private static void dfs(int depth) throws IOException {
+        if(depth >= M + 1){
+            for(int i = 1; i <= M; i++) {
+                bw.write(sequence[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for(int i = 1; i <= N; i++){
+            if(sequence[depth - 1] <= i){
+                sequence[depth] = i;
+                dfs(depth + 1);
+            }
+        }
+    }
+}
+
+
+
+
